327e39daf352b6217ea07d0cba9b6614c39a53c4
Fudong Bai <fudongbai@gmail.com>
2011-10-23 09:40:08 +0800

    add ARRAY_SIZE macro
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

---
+++ sizeofarray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define	ARRAY_SIZE(a)	sizeof(typeof(a)) / sizeof(typeof(a[0]))	
+
+int main()
+{
+	int a[] = {1, 2, 3, 4, 5, 6};
+
+	printf("size of array is: %d\n", ARRAY_SIZE(a));
+
+	return 0;
+}
