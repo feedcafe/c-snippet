ae46cff929f5f553311a98c4670e6de6f4e9449e
Fudong Bai <fudongbai@gmail.com>
2011-11-16 12:23:44 +0800

    send ctrl-c to app
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

---
+++ ctrl-c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <signal.h>
+
+unsigned char forever = 1;
+
+void cleanup(int sig)
+{
+	forever = 0;
+	printf("exiting test program\n");
+	(void) signal(SIGINT, SIG_DFL);
+}
+
+int main()
+{
+
+	(void)signal(SIGINT, &cleanup);		/* CTRL+C */
+	(void)signal(SIGABRT, &cleanup);
+	(void)signal(SIGTERM, &cleanup);
+
+	while (forever)
+		sleep(1), printf("sleeping ZZZzzzz\n");
+
+	return 0;
+}
