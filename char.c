6afc2de86219d6d478fe021d197e41dfa3613b3d
Fudong Bai <fudongbai@gmail.com>
2012-04-21 16:37:54 +0800

    explains why strcpy failed to copy strings
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- char.c
+++ char.c
@@ -5,7 +5,11 @@ int main()
 {
 	char *pmsg = "hello, world!";
 
-	/* result in Segmentation fault */
+	/* result in Segmentation fault
+	 * The address of string constant is allocated at build time
+	 * and pmsg is a pointer to that address, so its content cannot
+	 * be changed
+	 */
 	strcpy(pmsg, "hi, there."); 
 	printf("%s\n", pmsg);
 
