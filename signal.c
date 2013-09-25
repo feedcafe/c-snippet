8201548cb519d76327e3157ba697e23c43d9b002
Fudong Bai <fudongbai@gmail.com>
2013-09-25 22:11:41 +0800

    signal: cleanup
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- signal.c
+++ signal.c
@@ -13,7 +13,7 @@ int main(void)
 	if (signal(SIGINT, sig_handler) == SIG_ERR)
 		printf("\ncan't catch SIGINT\n");
 	// A long long wait so that we can easily issue a signal to this process
-	while (1) 
+	while (1)
 		sleep(1);
 	return 0;
 }
